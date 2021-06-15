7c8996529e9f2e339834d37b7c12f5841cac12e4
Akinade Mayowa <akinade.mayowa@gmail.com>
2021-06-15 16:05:24 +0100

    added user authentications

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,115 @@
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import React from 'react'
-import './NavBar.css'
+import ApiHandler from '../utils/ApiHandler'
+import AuthHandler from '../utils/AuthHandler'
 
+class NavBar extends Component {
 
-function NavBar() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">My Portfolio App</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Blog</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact_me">Contact Me</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/signup">Sign Up</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/logout">Logout</Link>
-                            </li>
-                        </ul>
-                    </div>
+    state = {
+        all_users: [],
+        login_status: false,
+        staff_status: false,
+        username: '',
+        email:'',
+    }
+
+    componentDidMount() {
+        this.loginCheck()
+    }
+
+    async allUsers() {
+        var users = new ApiHandler();
+        var response = await users.fetchAllUsers();
+        this.setState({ all_users: response.data })
+        var me = AuthHandler.getUsername();
+        this.setState({ username: me })
+        {this.state.all_users.map((user) => {
+            if (user.username == me) {
+                this.setState({ staff_status: user.is_superuser })
+            }
+        })}
+        //console.log(this.state.staff_status)
+    }
+
+    async loginCheck() {
+        var check = AuthHandler.loggedIn();
+        if (check) {
+            //console.log('user is active')
+            //console.log(AuthHandler.getLoginToken())
+            //return true;
+            this.setState({ login_status: true })
+        } else {
+            //console.log(AuthHandler.getLoginToken())
+            //console.log('user is not active')
+            //return false;
+            this.setState({ login_status: false })
+        }
+    }
+
+    render() {
+        var name = this.state.username;
+        if (this.state.login_status) {
+            return (
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                        <div className="container-fluid">
+                            <Link className="navbar-brand" to="/">My Portfolio App</Link>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNav">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/">Blog</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/contact_me">Contact Me</Link>
+                                    </li>
+                                    
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/logout">Logout</Link>
+                                    </li>                                
+                                </ul>
+                            </div>
+                            <h2>Welcome, {name}</h2>
+                        </div>
+                    </nav>
                 </div>
-            </nav>
-        </div>
-    )
+            )
+        } else {
+            return (
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                        <div className="container-fluid">
+                            <Link className="navbar-brand" to="/">My Portfolio App</Link>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNav">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/">Blog</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/contact_me">Contact Me</Link>
+                                    </li>
+                                    
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/signup">Sign Up</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/login">Login</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <h2>Welcome, Guest!</h2>
+                        </div>
+                    </nav>
+                </div>
+            )
+        }
+        
+    }
 }
 
 export default NavBar
--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,7 +1,9 @@
 class Config{
     static registerUrl = 'http://127.0.0.1:8000/portfolio/signup/';
+    static refreshApiUrl = "http://127.0.0.1:8000/api/token/refresh/";
     static loginUrl='http://127.0.0.1:8000/api/token/';
     static logoutPageUrl = "/logout";
+    static logoutUrl = "http://127.0.0.1:8000/portfolio/logout/"
     static listAllUsersUrl = 'http://127.0.0.1:8000/portfolio/list_users/';
 }
 
